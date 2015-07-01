df6dbf7cb6c4602566611402f130de0dedb1e5b3
Macartur Sousa <macartur.sc@gmail.com>
2015-06-30 22:05:34 -0300

    addded logic_operations

---
+++ include/logic_operation.h
@@ -0,0 +1,15 @@
+#ifndef LOGIC_OPERATION
+#define LOGIC_OPERATION
+
+
+void and_and();
+void or_or();
+void jump_bigger_than();
+void jump_less_than();
+void jump_bigger_or_equal();
+void jump_equal();
+void jump_different();
+void compare();
+void not_expresion();
+
+#endif
---
+++ src/logic_operation.c
@@ -0,0 +1,27 @@
+#include "logic_operation.h"
+
+
+void and_and();
+void or_or();
+void jump_bigger_than();
+void jump_less_than();
+void jump_bigger_or_equal();
+void jump_equal();
+void jump_different();
+void compare();
+void not_expresion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
